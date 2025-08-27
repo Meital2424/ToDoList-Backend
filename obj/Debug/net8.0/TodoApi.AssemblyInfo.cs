cf8c4956afe013275021079202b73781fe6f5dd6
MEITAL\meital <mitalc2424@gmail.com>
2025-08-27 18:43:02 +0300

    fix errors

--- obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27eee0a3f5e497f50867baeffae8642bf070243b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29e9b8cf993a097f9638c091f753d6bbcc950c73")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
